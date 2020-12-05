01c591c678df42a898948aeb8378faa012a13bcd
a132395 <67187730+a132395@users.noreply.github.com>
2020-12-05 15:55:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -162,12 +162,14 @@ async function updateDB(){
     const lastPageHref = $("div.pg > a.last").attr('href');
     response = await gotInstance.get(lastPageHref);
     console.log(lastPageHref);
-    const regex = /(forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page=)(\d*).html/;
+    //const regex = /(forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page=)(\d*).html/;
     //forum-36-
-    const match = lastPageHref.match(regex);
-    const forumPrefix = match[1];
-    const maxPageNumber = parseInt(match[2]);
-    console.log(forumPrefix,maxPageNumber);
+    //const match = lastPageHref.match(regex);
+    //const forumPrefix = match[1];
+    //const maxPageNumber = parseInt(match[2]);
+    //console.log(forumPrefix,maxPageNumber);
+    const forumPrefix = 'forum.php?mod=forumdisplay&fid=36&typeid=672&filter=typeid&typeid=672&page='
+    const maxPageNumber = 43
 
     console.log('Checking new posts...');
     const query_text = `INSERT INTO posts (url,title,postdate,downloaded)
